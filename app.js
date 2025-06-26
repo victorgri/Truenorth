378c986ddb21c4ebe02aca30b4c1a0d564e553af
Victor Hrygorash <viktor12041991@i.ua>
2025-06-27 01:06:15 +0300

    form and footer left

--- app.js
+++ app.js
@@ -1,9 +1,22 @@
 const navLinks = document.getElementById('nav-links');
 
 document.getElementById('menu-open').addEventListener('click', () => {
-    navLinks.classList.add('active');
+  navLinks.classList.add('active');
 });
 
 document.getElementById('menu-close').addEventListener('click', () => {
-    navLinks.classList.remove('active');
+  navLinks.classList.remove('active');
 });
+
+const acc = document.getElementsByClassName("accordion");
+for (let i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    const panel = this.nextElementSibling;
+    if (panel.style.display === "block") {
+      panel.style.display = "none";
+    } else {
+      panel.style.display = "block";
+    }
+  });
+}
